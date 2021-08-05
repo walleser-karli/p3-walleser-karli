a098d1c61ddf68c75dd8d6864d7ee4adac219861
walleser-karli <k.walleser@ufl.edu>
2021-08-05 14:58:19 -0400

    Added footer class with two properties and exporting as new function

---
+++ :src/:modules/footer.js
@@ -0,0 +1,8 @@
+class Footer {
+   constructor(copyrightYear, siteAuthor) {
+      this.copyrightYear = copyrightYear;
+      this.siteAuthor = siteAuthor;
+   }
+}
+
+export const footer = new Footer("© 2021", "Karli Walleser")
